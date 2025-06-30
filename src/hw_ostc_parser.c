b37005703f654b45726ec689f08215d006a25b78
Jef Driesen <jef@libdivecomputer.org>
2025-06-30 22:20:46 +0200

    Post release version bump to 0.10.0

--- src/hw_ostc_parser.c
+++ src/hw_ostc_parser.c
@@ -151,6 +151,9 @@ typedef struct hw_ostc_parser_t {
 	int last_scrubber_time_minutes;
 	bool scrubber_error_reported;
 	bool scrubber_warning_reported;
+	bool have_location;
+        float latitude;
+	float longitude;
 } hw_ostc_parser_t;
 
 static dc_status_t hw_ostc_parser_get_datetime (dc_parser_t *abstract, dc_datetime_t *datetime);
@@ -455,6 +458,10 @@ hw_ostc_parser_create_internal (dc_parser_t **out, dc_context_t *context, const 
 	parser->scrubber_error_reported = false;
 	parser->scrubber_warning_reported = false;
 	parser->serial = serial;
+	parser->have_location = false;
+	parser->latitude = 0.0;
+	parser->longitude = 0.0;
+
 
 	*out = (dc_parser_t *) parser;
 
@@ -747,6 +754,15 @@ hw_ostc_parser_get_field (dc_parser_t *abstract, dc_field_type_t type, unsigned 
 				return DC_STATUS_UNSUPPORTED;
 			}
 			decomodel->conservatism = 0;
+			break;
+		case DC_FIELD_LOCATION:
+			if (!parser->have_location)
+				return DC_STATUS_UNSUPPORTED;
+
+			location->latitude  = parser->latitude;
+			location->longitude = parser->longitude;
+			location->altitude  = 0.0;
+
 			break;
 		case DC_FIELD_STRING:
 			switch(flags) {
@@ -1247,6 +1263,61 @@ hw_ostc_parser_internal_foreach (hw_ostc_parser_t *parser, dc_sample_callback_t 
 
 					callback(DC_SAMPLE_EVENT, &sample, userdata);
 				}
+
+				offset += 2;
+				length -= 2;
+			}
+
+			// Compass heading
+			if (events & 0x0200) {
+				if (length < 2) {
+					ERROR (abstract->context, "Buffer overflow detected!");
+					return DC_STATUS_DATAFORMAT;
+				}
+
+				unsigned int value = array_uint16_le (data + offset);
+				unsigned int heading = value & 0x1FF;
+
+				if ((value & OSTC4_COMPASS_CLEARED) == 0) {
+					sample.bearing = heading;
+					if (callback) callback (DC_SAMPLE_BEARING, &sample, userdata);
+				}
+
+				offset += 2;
+				length -= 2;
+			}
+
+			// GNSS position
+			if (events & 0x0400) {
+				if (length < 8) {
+					ERROR (abstract->context, "Buffer overflow detected!");
+					return DC_STATUS_DATAFORMAT;
+				}
+
+				float longitude = array_float_le (data + offset);
+				float latitude  = array_float_le (data + offset + 4);
+
+				if (!parser->have_location) {
+					parser->latitude  = latitude;
+					parser->longitude = longitude;
+					parser->have_location = 1;
+				} else {
+					WARNING (abstract->context, "Multiple GNSS locations present.");
+				}
+
+				offset += 8;
+				length -= 8;
+			}
+
+			// Scrubber state
+			if (events & 0x0800) {
+				if (length < 2) {
+					ERROR (abstract->context, "Buffer overflow detected!");
+					return DC_STATUS_DATAFORMAT;
+				}
+
+				unsigned int value = array_uint16_le (data + offset);
+				int DC_ATTR_UNUSED remaining = (int) signextend (value & 0x0FFF, 12);
 
 				offset += 2;
 				length -= 2;
